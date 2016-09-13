58c628f76212d1d164068576a67c98bb711917a5
Mattias Liljenzin <mattias.liljenzin@gmail.com>
2016-09-13 22:40:41 +0200

    Working cost function plotting

--- ex1/testing.m
+++ ex1/testing.m
@@ -1,13 +1,36 @@
-function [result] = testing(theta0, theta1) 
+function J = testing(alpha) 
+
+	iterations = 50;
+	from = -1;
+	costs = [];
+	x = [];
+
+	for i=1:iterations
+		costs(i) = cost(0, from);
+		x(i) = from;
+		from += alpha;
+	end
+
+	plot(x, costs);
+	legend('Cost function - theta');
+	xlabel('theta'); 
+	ylabel('cost');
+
+end
+
+
+
+
+function [result] = cost(theta0, theta1) 
 	
-	inputData = [2; 2.5; 3.2; 5; 7.2; 8.5];
-	trainingData = [1.75; 2.95; 3.5; 4.75; 6; 9];
+	%inputData = [2; 2.5; 3.2; 5; 7.2; 8.5];
+	%trainingData = [1.75; 2.95; 3.5; 4.75; 6; 9];
+	%areas = [15; 27; 39; 45; 55; 75];
 
 	testData = [1,1; 2,2; 3,3];
-
-	areas = [15; 27; 39; 45; 55; 75];
 	predictedData = [];
 	hypothesis = [];
+	
 	m = length(testData);
 
 	for i = 1:m
@@ -21,7 +44,7 @@ function [result] = testing(theta0, theta1)
 		
 	%plot(predictedData, predictedData, 'rx', testData, testData, '-');
 	%plot(testData, hypothesis);
-	legend('Training data', 'Predicted data');
-	xlabel('kvm'); 
-	ylabel('kr');
+	%legend('Training data', 'Predicted data');
+	%xlabel('kvm'); 
+	%ylabel('kr');
 end
