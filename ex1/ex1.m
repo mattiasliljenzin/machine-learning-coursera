9c2fc6b281039d0f21a9e09450d484a0cbf30c7a
mattiasliljenzin <mattias.liljenzin@gmail.com>
2016-09-19 19:42:18 +0200

    reset

--- ex1/ex1.m
+++ ex1/ex1.m
@@ -32,8 +32,8 @@ fprintf('Running warmUpExercise ... \n');
 fprintf('5x5 Identity Matrix: \n');
 warmUpExercise()
 
-%fprintf('Program paused. Press enter to continue.\n');
-%pause;
+fprintf('Program paused. Press enter to continue.\n');
+pause;
 
 
 %% ======================= Part 2: Plotting =======================
@@ -60,9 +60,7 @@ iterations = 1500;
 alpha = 0.01;
 
 % compute and display initial cost
-j = computeCost(X, y, theta)
-fprintf('Testing - ');
-disp(j);
+computeCost(X, y, theta)
 
 % run gradient descent
 theta = gradientDescent(X, y, theta, alpha, iterations);
@@ -88,17 +86,17 @@ fprintf('For population = 70,000, we predict a profit of %f\n',...
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
-% %% ============= Part 4: Visualizing J(theta_0, theta_1) =============
-% fprintf('Visualizing J(theta_0, theta_1) ...\n')
+%% ============= Part 4: Visualizing J(theta_0, theta_1) =============
+fprintf('Visualizing J(theta_0, theta_1) ...\n')
 
-% % Grid over which we will calculate J
+% Grid over which we will calculate J
 theta0_vals = linspace(-10, 10, 100);
 theta1_vals = linspace(-1, 4, 100);
 
-% % initialize J_vals to a matrix of 0's
+% initialize J_vals to a matrix of 0's
 J_vals = zeros(length(theta0_vals), length(theta1_vals));
 
-% % Fill out J_vals
+% Fill out J_vals
 for i = 1:length(theta0_vals)
     for j = 1:length(theta1_vals)
 	  t = [theta0_vals(i); theta1_vals(j)];    
@@ -107,8 +105,8 @@ for i = 1:length(theta0_vals)
 end
 
 
-% % Because of the way meshgrids work in the surf command, we need to 
-% % transpose J_vals before calling surf, or else the axes will be flipped
+% Because of the way meshgrids work in the surf command, we need to 
+% transpose J_vals before calling surf, or else the axes will be flipped
 J_vals = J_vals';
 % Surface plot
 figure;
