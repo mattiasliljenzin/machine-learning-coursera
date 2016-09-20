9e9c5769988d57d599fa3a48055cb57cacd4ed3c
Mattias Liljenzin <mliljenzin@paypal.com>
2016-09-20 19:19:27 +0200

    gradientDescent working

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -1,6 +1,6 @@
 function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 %GRADIENTDESCENT Performs gradient descent to learn theta
-%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by 
+%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by
 %   taking num_iters gradient steps with learning rate alpha
 
 % Initialize some useful values
@@ -9,14 +9,30 @@ J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
 
-    h = X * theta;
-    s = sum(h - y);
-    theta = theta - alpha * (1/m) * s;
-    % ============================================================
+    A = X * theta - y;
+    s = A' * X;
+    theta = theta - alpha * (1/m) * s';
 
-    % Save the cost J in every iteration    
+    % for i=1:m
+    %     s = sum((h(X(i,2), theta(1), theta(2)) + h(X(i,1), theta(1), theta(2)))) - y(i);
+    %     t1 = theta(1) - alpha * (1/m) * sum(s);
+    %     t2 = theta(2) - alpha * (1/m) * sum(s * X(i,2));
+    %     theta(1) = t1;
+    %     theta(2) = t2;
+    % end
+
+    % A = X * theta - y;  % (m x 1 vector)
+    % delta = 1 / m * (A' * X)';  % ' ((n+1) x 1 vector)
+    % theta = theta - (alpha * delta);  % ' ((n+1) x 1 vector)
+
+
     J_history(iter) = computeCost(X, y, theta);
 
 end
 
+end
+
+
+function result = h(x, t0, t1)
+  result = t0 + t1 * x;
 end
