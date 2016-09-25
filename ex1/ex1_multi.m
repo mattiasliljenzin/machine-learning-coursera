c5ccc3d912b077672fc93750f106a59ef4d54959
Mattias Liljenzin <mliljenzin@paypal.com>
2016-09-25 21:36:48 +0200

    all extras done ml1

--- ex1/ex1_multi.m
+++ ex1/ex1_multi.m
@@ -104,7 +104,11 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+
+
+normSqrtFeet = (1650 - mu(:,1)) / sigma(:,1);
+normNumOfBeds = (3 - mu(:,2)) / sigma(:,2);
+price = theta(1) + (theta(2) * normNumOfBeds) + (theta(3) * normSqrtFeet);
 
 
 % ============================================================
@@ -146,10 +150,9 @@ fprintf('Theta computed from the normal equations: \n');
 fprintf(' %f \n', theta);
 fprintf('\n');
 
-
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = [1, 1650, 3] * theta; 
 
 
 % ============================================================
